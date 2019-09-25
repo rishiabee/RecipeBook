82a3e28db53389b34e4b59567b7ad9964a18076a
Rishi <42680217+rishiabee@users.noreply.github.com>
2019-09-25 23:32:22 +0400

    Showing a list of recipes

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'rishiabee-recipes-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes = [
+    new Recipe('Apple Tart', 'An Apple Tart recipe',
+      'https://cdn.pixabay.com/photo/2015/12/01/08/13/apple-pie-1071747_960_720.jpg'),
+    new Recipe('Chicken Burger', 'A chicken burger recipe',
+      'https://cdn.pixabay.com/photo/2016/04/17/12/10/american-1334633_960_720.jpg'),
+    new Recipe('Grilled Salmon', 'A grilled salmon recipe',
+      'https://cdn.pixabay.com/photo/2016/03/05/20/07/salmon-1238662_960_720.jpg')
+  ];
 
   constructor() { }
 
